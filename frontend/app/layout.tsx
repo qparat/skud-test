1adfb77984680c14cc7fa56a9cbd97809d1a5020
iambaimadi <96194946+iambaimadi@users.noreply.github.com>
2025-10-24 18:01:12 +0500

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body className="">
+      <body className="font-sans">
         <AuthProvider>
           <LayoutWrapper>
             {children}
