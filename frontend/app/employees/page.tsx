c555db0f6b4e8a1767ad07114733c33fcbfd9f79
iambaimadi <96194946+iambaimadi@users.noreply.github.com>
2025-10-28 11:31:42 +0500

    Update page.tsx

--- frontend/app/employees/page.tsx
+++ frontend/app/employees/page.tsx
@@ -107,7 +107,6 @@ export default function EmployeesPage() {
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
         <div>
           <h1 className="text-2xl font-semibold text-gray-900 flex items-center gap-2">
-            <Users className="h-7 w-7" />
               Список сотрудников
           </h1>
           <p className="mt-1 text-sm text-gray-600">
