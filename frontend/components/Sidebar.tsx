86c39c052be879b0a3824a3fbea9f9b20eeed877
iambaimadi <96194946+iambaimadi@users.noreply.github.com>
2025-10-30 13:19:39 +0500

    Update Sidebar.tsx

--- frontend/components/Sidebar.tsx
+++ frontend/components/Sidebar.tsx
@@ -20,8 +20,8 @@ const navigation = [
   { name: 'Службы', href: '/departments', icon: Building2, requireRole: 2 },
   { name: 'Должности', href: '/positions', icon: Briefcase, requireRole: 2 },
   { name: 'Исключения', href: '/exceptions', icon: Shield, requireRole: 2 },
-  { name: 'Пользователи', href: '/users', icon: UserCog, requireRole: 2 },
-    { name: 'Загрузка файлов', href: '/upload', icon: Upload, requireRole: 2  },
+  { name: 'Пользователи', href: '/users', icon: UserCog, requireRole: 1 },
+    { name: 'Загрузка файлов', href: '/upload', icon: Upload, requireRole: 1  },
 ]
 
 export function Sidebar() {
