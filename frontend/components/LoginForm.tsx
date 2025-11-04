b81c4e4e8ace600d5354ac48171522b0e92cca87
iambaimadi <96194946+iambaimadi@users.noreply.github.com>
2025-11-04 09:03:11 +0500

    asd

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -32,7 +32,7 @@ export default function LoginForm() {
     <div className="login">
       {/* Картинка слева */}
     <div className="img_login">
-      <img src="/SCUD_1.svg" alt="SCUD" className="" />
+      <img src="/3047867.jpg" alt="SCUD" className="" />
             </div>
       <div className="login_2 container">
         <form className="form_login" onSubmit={handleSubmit}>
@@ -84,7 +84,7 @@ export default function LoginForm() {
             <button
               type="submit"
               disabled={loading}
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed"
+              className="group relative w-full flex justify-center py-2 px-4 mt-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed"
             >
               {loading ? (
                 <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-white"></div>
