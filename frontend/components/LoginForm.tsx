4a84eaa1ebef478705d769389bf8e523650b4291
iambaimadi <96194946+iambaimadi@users.noreply.github.com>
2025-11-05 10:42:01 +0500

    12

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -32,7 +32,7 @@ export default function LoginForm() {
     <div className="login">
       {/* Картинка слева */}
     <div className="img_login">
-      <img src="/30478678.jpg" alt="SCUD" className="" />
+      <img src="/komp-uternaa-bezopasnost-s-zamkom-dla-vhoda-i-parola.jpg" alt="SCUD" className="" />
             </div>
       <div className="login_2 container">
         <form className="form_login" onSubmit={handleSubmit}>
